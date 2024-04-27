2e63c97f921319a9f39153239f947e4c11b64219
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-04-27 19:11:09 -0400

    Complete exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (Number.isInteger(year) && year % 4 === 0) {
+        if (year % 100 === 0) {
+            return year % 400 === 0 ? true : false;
+        }
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
