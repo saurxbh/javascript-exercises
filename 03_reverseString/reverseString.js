8f1bdbb5fc23c57ae6446e56767d8ac9931ba6b7
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-04-27 01:20:18 -0400

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let string_array = [];
+    for (let i = 0; i < string.length; i++) {
+        string_array.push(string[i]);
+    }
+    string_array = string_array.reverse();
+    return string_array.join('');
 };
 
 // Do not edit below this line
