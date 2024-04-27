e64c2383eb26f0e0880b2cd8b14881f7c4204c13
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-04-27 18:29:27 -0400

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...rest) {
+    for (const element of rest) {
+        while (array.includes(element)) {
+            index = array.indexOf(element);
+            array = array.slice(0, index).concat(array.slice(index + 1)).concat(array.at(index));
+            array.pop();
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
