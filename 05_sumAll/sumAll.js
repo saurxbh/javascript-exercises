f32c27d207196b2031f66bc855af5e7c5c338cc5
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-04-27 18:42:25 -0400

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a != "number" || a < 0 || typeof b != "number" || b < 0) {
+        return "ERROR"
+    } else {
+        let smaller = a < b ? a : b;
+        let bigger = smaller === a ? b : a;
+        let sum = 0;
+        for (let i = smaller; i <= bigger; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
