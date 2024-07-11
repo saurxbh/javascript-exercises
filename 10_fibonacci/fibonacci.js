262f41e916357766375b34e3bd6d204228326a98
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-07-11 16:19:47 -0400

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(countArg) {
+    let count;
+    if (typeof countArg !== 'number') {
+        count = parseInt(countArg);
+    } else {
+        count = countArg;
+    }
 
+    if (count < 0) return "OOPS";
+    if (count == 0) return 0;
+    if (count == 1) return 1;
+
+    let firstPrev = 1;
+    let secondPrev = 0;
+    
+    for(let i = 2; i <= count; i++) {
+        current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return current;
 };
 
 // Do not edit below this line
