a0e30b2fc4849662fcb0a7c4ad0f2cb3f391748e
saurxbh <36542272+saurxbh@users.noreply.github.com>
2024-07-11 16:09:28 -0400

    Complete 09_palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
 const palindromes = function(string) {
-    const reversedString = string.split('').reverse().join('');
-    return string === reversedString;
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const cleanedString = string
+        .toLowerCase()
+        .split('')
+        .filter(character => alphanumerical.includes(character))
+        .join('')
+
+    const reversedString = cleanedString.split('').reverse().join('');
+    return cleanedString === reversedString;
 };
 
 // Do not edit below this line
